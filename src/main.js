ab6f5901806967815271a786599c90bf5f761e82
saturnSolar <saturnsolar1093@gmail.com>
2026-01-02 23:56:24 +0800

    Changed element variable to container instead of the input text..

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import {animate, stagger} from 'animejs'
 
-const $text = document.querySelector('.text');
+const $container = document.querySelector('.container');
 
-animate($text,
+animate($container,
   {
     translateY: [
       {from: 100}
@@ -21,7 +21,7 @@ animate($text,
   }
 );
 
-let anim = animate($text,
+let anim = animate($container,
   {
     boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.43)',
     duration: 200,
@@ -29,8 +29,8 @@ let anim = animate($text,
   }
 )
 
-$text.addEventListener('focus', () => anim.play());
+$container.querySelector('.text').addEventListener('focus', () => anim.play());
 
-$text.addEventListener('blur', () => anim.reverse());
+$container.querySelector('.text').addEventListener('blur', () => anim.reverse());
 
 // setupCounter(document.querySelector('#counter'))
