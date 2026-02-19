b367f39cda40bbc90c8553a7f11079979a582a14
saturnSolar <saturnsolar1093@gmail.com>
2026-02-20 01:39:21 +0800

    Separated actual data processing.

---
+++ src/processScreen.js
@@ -0,0 +1,14 @@
+import { addProcessText } from "./inputScreen.js";
+import { getToken } from "./login.js";
+import { getUser } from "./fetchData.js";
+
+export async function processInput(data) {
+  const text1 = await addProcessText("Fetching token...");
+  const token = await getToken();
+  const text2 = await addProcessText("Checking user...");
+  const user = await getUser(data, token);
+  console.log(user);
+  const text3 = await addProcessText("Generating...");
+  await addProcessText(`Success, hello ${user.username}!`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+}
