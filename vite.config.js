1a59cf7d97f3bfeca3d72cc8a29e714905ef4f40
saturnSolar <saturnsolar1093@gmail.com>
2026-02-20 01:37:11 +0800

    Added dev proxy for testing backend

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/osuWrapped/', // Replace with your actual repo name!
-})
+  base: "/osuWrapped/", // Replace with your actual repo name!
+
+  server: {
+    proxy: {
+      "/oauth": {
+        target: "https://osu.ppy.sh",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/oauth/, "/oauth"),
+      },
+      "/api": {
+        target: "https://osu.ppy.sh",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api/v2"),
+      },
+    },
+  },
+});
